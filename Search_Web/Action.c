322f67d833f823adbf2497a476a1e0aef4a951b3
Mark Steffensen <mark.steffensen@microfocus.com>
2021-05-04 16:01:27 +0000

    Initial commit

---
+++ Search_Web/Action.c
@@ -0,0 +1,239 @@
+Action()
+{
+	
+	// This script does the following:
+	// 1. Open AOS URL website (default AOS_URL parameter = http://nimbusserver.aos.com:8000/  )
+	// 2. Click the Search icon (magnifying glass)
+	// 3. Search for Roar and hit Return
+	// 4. Click on Roar mini
+	// 5. Click on the Search icon (magnifying glass)
+	// 6. Search for Bose and hit Return
+	// 7. Click on Bose Wireless
+	// 8. Click Home
+	//
+	// Two transactions are available - AOS_Home, AOS_Search
+	
+	web_add_cookie("userCart=%7B%22userId%22%3A-1%2C%22productsInCart%22%3A%5B%5D%7D; DOMAIN=nimbusserver.aos.com");
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_start_transaction("AOS_Home");
+	
+	web_url("nimbusserver.aos.com:8000", 
+		"URL={AOS_URL}", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/main.min.js", ENDITEM, 
+		"Url=/services.properties", ENDITEM, 
+		"Url=/css/images/closeDark.png", ENDITEM, 
+		"Url=/css/images/logo.png", ENDITEM, 
+		"Url=/css/fonts/roboto_regular_macroman/Roboto-Regular-webfont.woff", "Referer={AOS_URL}css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_medium_macroman/Roboto-Medium-webfont.woff", "Referer={AOS_URL}css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_light_macroman/Roboto-Light-webfont.woff", "Referer={AOS_URL}css/main.min.css", ENDITEM, 
+		"Url=/css/images/arrow_right.png", "Referer={AOS_URL}css/main.min.css", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=headphones", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=laptops", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=mice", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=tablets", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=speakers", ENDITEM, 
+		"Url=/css/fonts/roboto_bold_macroman/Roboto-Bold-webfont.woff", "Referer={AOS_URL}css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_thin_macroman/Roboto-Thin-webfont.woff", "Referer={AOS_URL}css/main.min.css", ENDITEM, 
+		"Url=/css/images/Special-offer.jpg", ENDITEM, 
+		"Url=/css/images/GoUp.png", ENDITEM, 
+		"Url=/css/images/twitter.png", ENDITEM, 
+		"Url=/css/images/facebook.png", ENDITEM, 
+		"Url=/css/images/linkedin.png", ENDITEM, 
+		"Url=/css/images/Banner1.jpg", ENDITEM, 
+		"Url=/css/images/Banner2.jpg", ENDITEM, 
+		"Url=/css/images/Banner3.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item3.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item2.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item1.jpg", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=1400", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=3300", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4100", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=2100", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=1245", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5100", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=1249", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=2300", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5700", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4600", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=1250", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4300", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4400", ENDITEM, 
+		"Url=/css/images/Filter.png", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4200", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4700", ENDITEM, 
+		"Url=/css/images/Check.png", "Referer={AOS_URL}css/main.min.css", ENDITEM, 
+		LAST);
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_auto_header("Origin", 
+		"http://nimbusserver.aos.com:8000");
+
+	web_url("ALL", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/DemoAppConfig/parameters/by_tool/ALL", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_header("Access-Control-Request-Headers", 
+		"content-type,soapaction");
+
+	web_add_header("Access-Control-Request-Method", 
+		"POST");
+
+	web_add_header("Sec-Fetch-Mode", 
+		"cors");
+
+	lr_think_time(8);
+
+	web_custom_request("GetAccountConfigurationRequest", 
+		"URL=http://nimbusserver.aos.com:8001//accountservice/ws/GetAccountConfigurationRequest", 
+		"Method=OPTIONS", 
+		"Resource=0", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_header("SOAPAction", 
+		"com.advantage.online.store.accountserviceGetAccountConfigurationRequest");
+
+	web_custom_request("GetAccountConfigurationRequest_2", 
+		"URL=http://nimbusserver.aos.com:8001//accountservice/ws/GetAccountConfigurationRequest", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=text/xml", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		"EncType=text/xml; charset=UTF-8", 
+		"Body=<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><soap:Body><GetAccountConfigurationRequest xmlns=\"com.advantage.online.store.accountservice\"></GetAccountConfigurationRequest></soap:Body></soap:Envelope>", 
+		LAST);
+
+	web_url("categories", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/categories", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("search", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/deals/search?dealOfTheDay=true", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t6.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_cookie("_ga=GA1.2.1598072456.1620081396; DOMAIN=nimbusserver.aos.com");
+
+	web_add_cookie("_gid=GA1.2.2127572492.1620081396; DOMAIN=nimbusserver.aos.com");
+
+	web_add_cookie("_gat=1; DOMAIN=nimbusserver.aos.com");
+
+	web_revert_auto_header("Origin");
+
+	web_url("popularProducts.json", 
+		"URL={AOS_URL}app/tempFiles/popularProducts.json", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("home-page.html", 
+		"URL={AOS_URL}app/views/home-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t8.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("AOS_Home",LR_AUTO);
+
+	lr_start_transaction("AOS_Search");
+
+	web_add_auto_header("Origin", 
+		"http://nimbusserver.aos.com:8000");
+
+	lr_think_time(8);
+
+	web_url("search_2", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/products/search?name=R&quantityPerEachCategory=10", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t9.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("search_3", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/products/search?name=R&quantityPerEachCategory=10", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t10.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("all_data", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/categories/all_data", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t11.inf", 
+		"Mode=HTML", 
+		LAST);
+	
+	lr_think_time(8);
+
+	web_url("all_data_2", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/categories/all_data", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t12.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_revert_auto_header("Origin");
+
+	web_url("search-page.html", 
+		"URL={AOS_URL}app/views/search-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t13.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("product-page.html", 
+		"URL={AOS_URL}app/views/product-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer={AOS_URL}", 
+		"Snapshot=t14.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("AOS_Search",LR_AUTO);
+
+	return 0;
+}
